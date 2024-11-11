3b1388c6ed4167eb70b99fc48e86a984d9609da8
Søren Sandgaard <sorsa@dtu.dk>
2024-11-11 15:53:46 +0100

    A few python edits

--- entrypoint_method.py
+++ entrypoint_method.py
@@ -21,6 +21,17 @@ def run_method(output_dir, name, input_files, parameters):
     # Create the output directory if it doesn't exist
     os.makedirs(output_dir, exist_ok=True)
 
+    # Run Cellranger ctrl
+    print("Testing")
+    # Run Cellranger case 1
+    print("Testing 2")
+    # Run Bamboozle case
+    print("Testing 3")
+    # Run Bamtofastq case
+    print("Testing 4")
+    # Run Cellranger case 2
+    print("Testing 5")
+
     content = concatenate_input_content(input_files)
 
     method_mapping_file = os.path.join(output_dir, f'{name}.model.out.gz')
@@ -37,23 +48,28 @@ def main():
     # Add arguments
     parser.add_argument('--output_dir', type=str, help='output directory where method will store results.')
     parser.add_argument('--name', type=str, help='name of the dataset')
-    parser.add_argument('--process.filtered', type=str, help='optional input file #1.', required=False)
-    parser.add_argument('--data.counts', type=str, help='optional input file #1.', required=False)
-    parser.add_argument('--data.meta', type=str, help='input file #2.')
-    parser.add_argument('--data.data_specific_params', type=str, help='input file #3.')
+    parser.add_argument('--R1.counts',type=str, help='input file #1')
+    parser.add_argument('--R2.counts',type=str, help='input file #1')
+#    parser.add_argument('--process.filtered', type=str, help='optional input file #1.', required=False)
+#    parser.add_argument('--data.counts', type=str, help='optional input file #1.', required=False)
+#    parser.add_argument('--data.meta', type=str, help='input file #2.')
+#    parser.add_argument('--data.data_specific_params', type=str, help='input file #3.')
 
     # Parse arguments
     args, extra_arguments = parser.parse_known_args()
 
-    process_filtered_input = getattr(args, 'process.filtered')
-    data_counts_input = getattr(args, 'data.counts')
-    data_meta_input = getattr(args, 'data.meta')
-    data_params_input = getattr(args, 'data.data_specific_params')
+    R1_input = getattr(args, 'R1.counts')
+    R2_input = getattr(args, 'R2.counts')
 
-    assert process_filtered_input is not None or data_counts_input is not None, "At least one of the values must not be None"
-    data_counts_input = process_filtered_input if process_filtered_input else data_counts_input
+#    process_filtered_input = getattr(args, 'process.filtered')
+#    data_counts_input = getattr(args, 'data.counts')
+#    data_meta_input = getattr(args, 'data.meta')
+#    data_params_input = getattr(args, 'data.data_specific_params')
 
-    input_files = [data_counts_input, data_meta_input, data_params_input]
+#    assert process_filtered_input is not None or data_counts_input is not None, "At least one of the values must not be None"
+#    data_counts_input = process_filtered_input if process_filtered_input else data_counts_input
+
+    input_files = [R1_input, R2_input]
 
     run_method(args.output_dir, args.name, input_files, extra_arguments)
 
