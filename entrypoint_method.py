4d198885e928d3131402531e48f636f92d1cea17
Søren Sandgaard <sorsa@dtu.dk>
2024-11-15 13:34:01 +0100

    Added cellranger functionality

--- entrypoint_method.py
+++ entrypoint_method.py
@@ -36,7 +36,7 @@ def run_method(output_dir, name, input_files, parameters):
     # --transcriptome $transcriptome_ref --create-bam true --expect-cells 15000 --localcores 28 --localmem 128
 
     # Run Cellranger case 1
-    a = subprocess.run(["ls","-lh"],capture_output=True,text=True)
+    subprocess.run(cr_command_1.split(),capture_output=True,text=True)
 
     content = f"This is the output from subprocess.run\n"
     content += a.stdout
