bad56643754fa483c7093495dc1dad63649e4a17
Søren Sandgaard <sorsa@dtu.dk>
2024-11-22 14:24:15 +0100

    Added handling of bam input file

--- entrypoint_method.py
+++ entrypoint_method.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import subprocess
 
-def run_method(output_dir, name, input_files, parameters):
+def run_method(output_dir, name, input_file, parameters):
     # Create the output directory if it doesn't exist
     os.makedirs(output_dir, exist_ok=True)
     log_file = os.path.join(output_dir, f'{name}.log.txt')
@@ -51,8 +51,7 @@ def main():
     # Add arguments
     parser.add_argument('--output_dir', type=str, help='output directory where method will store results.')
     parser.add_argument('--name', type=str, help='name of the dataset')
-    parser.add_argument('--R1.counts',type=str, help='input file #1')
-    parser.add_argument('--R2.counts',type=str, help='input file #1')
+    parser.add_argument('--init_bam',type=str, help='anonymized_bam_file')
 #    parser.add_argument('--process.filtered', type=str, help='optional input file #1.', required=False)
 #    parser.add_argument('--data.counts', type=str, help='optional input file #1.', required=False)
 #    parser.add_argument('--data.meta', type=str, help='input file #2.')
@@ -61,9 +60,7 @@ def main():
     # Parse arguments
     args, extra_arguments = parser.parse_known_args()
 
-    R1_input = getattr(args, 'R1.counts')
-    R2_input = getattr(args, 'R2.counts')
-    fastq_paths = os.path.dirname(R1_input) + f"/"
+    bam_file = getattr(args, 'init_bam')
 
 #    process_filtered_input = getattr(args, 'process.filtered')
 #    data_counts_input = getattr(args, 'data.counts')
@@ -73,10 +70,8 @@ def main():
 #    assert process_filtered_input is not None or data_counts_input is not None, "At least one of the values must not be None"
 #    data_counts_input = process_filtered_input if process_filtered_input else data_counts_input
 
-    input_files = [R1_input, R2_input]
-
     # run_method(args.output_dir, args.name, input_files, extra_arguments)
-    run_method(args.output_dir, args.name, fastq_paths, extra_arguments)
+    run_method(args.output_dir, args.name, bam_file, extra_arguments)
 
 
 if __name__ == "__main__":
