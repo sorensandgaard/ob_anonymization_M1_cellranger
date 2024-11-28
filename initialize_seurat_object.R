8cd4571e4d9a9e6eb206bc500a3a60f558cc114d
Søren Sandgaard <sorsa@dtu.dk>
2024-11-28 14:56:36 +0100

    debugging R script

--- initialize_seurat_object.R
+++ initialize_seurat_object.R
@@ -10,7 +10,7 @@ a <- data.frame(t1 = 1:5)
 a$t2 <- output_dir
 a$t3 <- data_input_dir
 
-data <- Read10X(data.dir = data_input_dir)
-seurat_object = CreateSeuratObject(counts = data)
+# data <- Read10X(data.dir = data_input_dir)
+# seurat_object = CreateSeuratObject(counts = data)
 
-saveRDS(seurat_object,file = "test.rds")
+saveRDS(a,file = "test.rds")
